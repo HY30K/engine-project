c71c7942559bdcf90f4b6301d204d8dc37b68113
Hyeok <98952450+Hyeok-06@users.noreply.github.com>
2022-11-08 15:17:05 +0900

    오프닝 씬

--- Engine-pj/Assets/01_Script/JaeHee/AnimEvent/AnimationEvent.cs
+++ Engine-pj/Assets/01_Script/JaeHee/AnimEvent/AnimationEvent.cs
@@ -32,7 +32,7 @@ public class AnimationEvent : MonoBehaviour
     {
         hitBox.enabled = true;
         playerBody.size = new Vector2(0.75f, 1.9f);
-        playerBody.offset = new Vector2(-0.02f, -0.5f);
+        playerBody.offset = new Vector2(-0.02f, -0.05f);
         _animator.SetBool("Roll", false);
     }
 
