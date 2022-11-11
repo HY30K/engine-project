01a9757ac93a0e67a7cf0a24bb8e0eec26725edc
ahslxhvhapt12 <likjhee1@gmail.com>
2022-11-11 10:02:17 +0900

    Inven
    Fix_Inven

--- Engine-pj/Assets/01_Script/JaeHee/Inven2/ItemSO.cs
+++ Engine-pj/Assets/01_Script/JaeHee/Inven2/ItemSO.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SEH00N
+[CreateAssetMenu(fileName = "ItemSO")]
+public class ItemSO : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "ItemSO")]
-    public class ItemSO : ScriptableObject 
-    {
-        [SerializeField] string itemName;
-        public string ItemName => itemName;
+    [SerializeField] string itemName;
+    public string ItemName => itemName;
 
-        [SerializeField] Sprite itemImage;
-        public Sprite ItemImage => itemImage;    
+    [SerializeField] Sprite itemImage;
+    public Sprite ItemImage => itemImage;
 
-        [SerializeField] int stackCount;
-        public int StackCount => stackCount;
-    }
+    [SerializeField] int stackCount;
+    public int StackCount => stackCount;
 }
+
