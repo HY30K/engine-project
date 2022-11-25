57320a3a1b2988ab9680bbb8c0795ce2a63094b1
ahslxhvhapt12 <likjhee1@gmail.com>
2022-11-25 21:26:22 +0900

    Fix
    FIx_Main

--- Engine-pj/Assets/01_Script/Junseong/MiningParticle.cs
+++ Engine-pj/Assets/01_Script/Junseong/MiningParticle.cs
@@ -2,11 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class MiningParticle : PoolAbleMono
 {
+    private AudioSource _audioSource;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void DestroyMiningParticle()
+    {
+        PoolManager.Instance.Push(this);
+    }
+
+    public void SetPositionAndRotation(Vector3 pos, Quaternion rot)
+    {
+        transform.SetPositionAndRotation(pos, rot);
+        //_audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public override void Init()
     {
-        
+
     }
 }
